7f8fe2c4f36c893d3914858332ec35f37339aee4
SURAJ C S <surajcs@outlook.com>
2021-06-13 16:37:02 +0530

    Added basic Error Handling

--- TvDataDownloder.py
+++ TvDataDownloder.py
@@ -7,23 +7,26 @@ from time import sleep
 username,password=get_cred('tradingview')
 tv=TvDatafeed(username=username,password=password)
 
-path='C:\\Users\\suraj\\Desktop\\EOD DATA FROM TV'
+path='C:\\Users\\suraj\\Desktop\\EOD DATA FROM TV 1000'
 if not os.path.exists(path):
     os.makedirs(path)
 
-f=open("stocklist.txt","r")
+f=open("symbols1000.txt","r")
 ticker=f.readlines()
 
 num_lines=len(ticker)
 
 bar = progressbar.ProgressBar(maxval=num_lines,widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 bar.start()
-
 for x in range(num_lines):
-     df = tv.get_hist(ticker[x].strip(),'NSE',interval=Interval.in_daily,n_bars=10000)
-     df.to_csv(path +'\\%s.csv'%ticker[x].strip())
-     bar.update(x)
-    
+    try:
+        df = tv.get_hist(ticker[x].strip(),'NSE',interval=Interval.in_daily,n_bars=10000)
+        print('Downloading........'+ticker[x].strip())
+        df.to_csv(path +'\\%s.csv'%ticker[x].strip())
+        bar.update(x)
+    except:
+        print("Not able to download @@@"+ticker[x].strip()) 
+        continue       
 f.close()
 bar.finish()
 
